27f5cb30e7bf2fa3447dad630b581656052671ab
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-04-16 01:04:55 +0500

    add controller started

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const Router = express.Router();
 const Home = require('../Controllers/HomeController');
 const Api = require('../Controllers/ApiController');
+const Add = require('../Controllers/AddController');
+
 
 //API route
 Router.get('/api', Api.get);
 
 //Home route
 Router.get('/', Home.get);
+
+Router.get('/add', Add.get);
+
 
 //////////////////not found
 Router.all('*', function (req, res) {
