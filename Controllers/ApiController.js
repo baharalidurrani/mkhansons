3d85079b6ab4431aeaf11a663d5e7a7d756021ba
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-04-16 00:05:08 +0500

    Initial commit

---
+++ Controllers/ApiController.js
@@ -0,0 +1,2 @@
+exports.get = (req, res) => {
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const URI = process.env.MLAB || 'mongodb://localhost:27017/mkhansons';
+
+//setting template engine
+app.set('view engine', 'ejs');
+//static files
+app.use(express.static('./views/assets'));
+
+//middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//connection with mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect(URI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Database Connnected');
+}).catch((err) => {
+    console.log(err);
+});
+
+//start web server
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+//fetch routes
+app.use(require('./Routes/Routes'));
