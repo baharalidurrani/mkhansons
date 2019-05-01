e9bf87ac0f9f5d138247d87aba504d1ee8746b72
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-05-01 15:40:14 +0500

    bill controller and printing done

--- Controllers/BillController.js
+++ Controllers/BillController.js
@@ -6,26 +6,49 @@ exports.get = (req, res) => {
 
 exports.post = (req, res) => {
     // console.log(req.body);
+    var billitems = [];
+    var totalbill = 0;
+    var shopname = req.body.SHOP;
+
     let today = new Date().toLocaleDateString();
-    console.log(today);
+
     for (let index = 0; index < req.body.PRODUCT.length; index++) {
         if (!req.body.QUANTITY[index])
             continue;
+
         const id = req.body.PRODUCT[index];
         const qty = req.body.QUANTITY[index];
+        const ttl = req.body.QUANTITY[index] * req.body.PRICE[index]
+        totalbill += ttl;
+
+        billitems.push({
+            cname: req.body.CNAME[index],
+            name: req.body.NAME[index],
+            price: req.body.PRICE[index],
+            quantity: req.body.QUANTITY[index],
+            total: ttl
+        });
+
 
         ProductModel.findById(id).then((doc) => {
             if (doc._lastDate == today) {
                 let sold = doc._sold + qty;
-                ProductModel.findOneAndUpdate({ _id: id }, { _sold: sold }, { new: true }).then((data) => {
-                    console.log(data);
-                });
+                ProductModel.findOneAndUpdate({ _id: id }, { _sold: sold }, { new: true });
             }
             else {
-                ProductModel.findOneAndUpdate({ _id: id }, { _lastDate: today, _sold: qty }, { new: true }).then((data) => {
-                    console.log(data);
-                });
+                ProductModel.findOneAndUpdate({ _id: id }, { _lastDate: today, _sold: qty }, { new: true });
             }
         });
     }
+
+    // console.log("/////////////////////////////////////////////////");
+    // console.log(shopname);
+    // console.log(billitems);
+    // console.log(totalbill);
+
+    res.render('Bill', {
+        data: billitems,
+        total: totalbill,
+        shop: shopname
+    });
 }
