65954dc7fa309e6019665cfc13a186ad8e83351a
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-05-04 00:51:22 +0500

    summary finalized

--- Controllers/SummaryController.js
+++ Controllers/SummaryController.js
@@ -4,10 +4,8 @@ exports.get = (req, res) => {
 
     let today = new Date().toLocaleDateString();
 
-    ProductModel.find({ _lastDate: today }).populate('_company').then((docs) => {
-        console.log(docs);
-        res.send(docs);
+    ProductModel.find({ _lastDate: today }).populate('_company').sort('_company').then((docs) => {
+        res.render('Summary.ejs', { data: docs });
     });
 
-    // res.render('Bill.ejs');
 }
