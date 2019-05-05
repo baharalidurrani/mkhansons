054b8579005a22e108d69a59385b03712f2dd554
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-05-06 02:28:21 +0500

    view css corrected

--- Controllers/SummaryController.js
+++ Controllers/SummaryController.js
@@ -5,7 +5,10 @@ exports.get = (req, res) => {
     let today = new Date().toLocaleDateString();
 
     ProductModel.find({ _lastDate: today }).populate('_company').sort('_company').then((docs) => {
-        res.render('Summary.ejs', { data: docs });
+        res.render('Summary.ejs', {
+            data: docs,
+            date: new Date()
+        });
     });
 
 }
